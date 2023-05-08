81c7778234617e33bee16318c85b03707f4a9925
messlav <slav.pirogow@yandex.ru>
2023-05-08 13:23:08 +0300

    prefreeze

--- utils/utils.py
+++ utils/utils.py
@@ -1,17 +1,21 @@
 import numpy as np
 import torch
 import random
+import os
 
 
 def set_random_seed(seed):
     torch.manual_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
     if torch.cuda.is_available():
         torch.backends.cudnn.deterministic = True
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.benchmark = True
 
+    # torch.use_deterministic_algorithms(True)
+    # os.environ["CUBLAS_WORKSPACE_CONFIG"] = ':4096:8'
     # torch.set_num_threads(1)
     # torch.set_num_interop_threads(1)
